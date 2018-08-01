c1f3b8fcef646cb249e372967ac47cea387b3e31
yonggwan07 <yk9007@gmail.com>
2018-08-01 17:01:38 +0900

    지우개 추가

--- app/src/main/java/jyh/test/android/mie_project/DrawingActivity.java
+++ app/src/main/java/jyh/test/android/mie_project/DrawingActivity.java
@@ -4,10 +4,10 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class DrawingActivity extends AppCompatActivity {
 
@@ -21,12 +21,13 @@ public class DrawingActivity extends AppCompatActivity {
 
         FrameLayout frame = findViewById(R.id.frame);
 
-        final Button btnUndo, btnRedo, btnColorPickerD, btnColor;
+        Button btnUndo, btnRedo, btnColorPickerD, btnPencilD, btnEraserD;
 
         btnUndo = findViewById(R.id.btnUndo);
         btnRedo = findViewById(R.id.btnRedo);
         btnColorPickerD = findViewById(R.id.btnColorPickerD);
-        btnColor = findViewById(R.id.btnColor);
+        btnPencilD = findViewById(R.id.btnPencilD);
+        btnEraserD = findViewById(R.id.btnEraserD);
 
         mDrawView = new DrawView(this);
         frame.addView(mDrawView);
@@ -54,10 +55,18 @@ public class DrawingActivity extends AppCompatActivity {
             }
         });
 
-        btnColor.setOnClickListener(new View.OnClickListener() {
+        btnPencilD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 colorPicker.showPicker(view);
+            }
+        });
+
+        btnEraserD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "지우개가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                mDrawView.setCurrentColor(Color.WHITE);
             }
         });
 
