0d6849ff914c150899f5325c7a1dbfbf9933bf98
yonggwan07 <yk9007@gmail.com>
2018-08-02 17:00:23 +0900

    카메라 사진 회전 방지

--- app/src/main/java/jyh/test/android/mie_project/DrawingActivity.java
+++ app/src/main/java/jyh/test/android/mie_project/DrawingActivity.java
@@ -142,14 +142,16 @@ public class DrawingActivity extends AppCompatActivity {
                 case TAKE_CAMERA:
                     if(data != null) {
                         Matrix matrix = new Matrix();
-                        matrix.postRotate(90);
 
                         Bitmap origin = (Bitmap) data.getExtras().get("data");
+
+                        if(origin.getWidth() > origin.getHeight())
+                            matrix.postRotate(90);
+
                         Bitmap scaledBitmap = Bitmap.createScaledBitmap(origin, frame.getMeasuredHeight(), frame.getMeasuredWidth(), true);
                         Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
 
                         mDrawView.setCameraPicture(rotatedBitmap);
-
                     }
 
                     break;
